f2b5ccddc971726a345878a3437ecfe2a5f956cf
le625202 <le625202@gmail.com>
2024-04-01 22:07:27 +0900

    20240401/이정석

--- src/main/java/com/suje/dao/main/MainRealTimeReviewDaoImpl.java
+++ src/main/java/com/suje/dao/main/MainRealTimeReviewDaoImpl.java
@@ -11,32 +11,34 @@ import org.springframework.stereotype.Repository;
 import com.suje.domain.main.MainRealTimeReviewVO;
 
 @Repository("MainRealTimeReviewDao")
-public class MainRealTimeReviewDaoImpl implements  MainRealTimeReviewDao{
+public class MainRealTimeReviewDaoImpl implements MainRealTimeReviewDao {
 
-	private static final Logger logger = LoggerFactory.getLogger(MainRealTimeReviewDaoImpl.class);
-	
-	
-	
-	@Autowired
-    private SqlSession sqlSession; 
-	
-	@Override
+    private static final Logger logger = LoggerFactory.getLogger(MainRealTimeReviewDaoImpl.class);
+
+    @Autowired
+    private SqlSession sqlSession;
+
+    @Override
     public List<MainRealTimeReviewVO> getreviewList(MainRealTimeReviewVO vo) {
         logger.info("실시간후기 목록 가져오기 - sqlSession");
         return sqlSession.selectList("getreviewList", vo);
     }
-    
+
     @Override
     public int getTotalCountPage() {
         logger.info("실시간후기 전체 행 수 추출(페이징) - sqlSession");
-        return sqlSession.selectOne("getTotalCountPage");
-    }    
+        return sqlSession.selectOne("MainRealTimeReviewDao.getTotalCountPage");
+    }
+
     @Override
     public void updateLikeCount(int reviewId) {
         logger.info("좋아요 수 업데이트 - 서비스");
         sqlSession.update("updateLikeCount", reviewId);
     }
-	
-	
-	
+
+    @Override
+    public int getUpdatedLikes(int reviewId) {
+        logger.info("좋아요 수 업데이트 후 좋아요 뷰 - 서비스");
+        return sqlSession.selectOne("getUpdatedLikes", reviewId);
+    }
 }
